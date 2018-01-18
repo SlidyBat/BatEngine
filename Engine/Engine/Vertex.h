04248461b5787b1f186849391ba8f145435e3380
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2018-01-19 02:11:00 +1100

    Move input layour data to Vertex class

--- Engine/Engine/Vertex.h
+++ Engine/Engine/Vertex.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <d3d11.h>
 #include <DirectXMath.h>
 
 struct Vertex
@@ -11,4 +12,11 @@ struct Vertex
 	{}
 	DirectX::XMFLOAT3 position;
 	DirectX::XMFLOAT4 colour;
+
+	static constexpr int Inputs = 2;
+	static constexpr D3D11_INPUT_ELEMENT_DESC InputLayout[Inputs] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, 0,                            D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
 };
