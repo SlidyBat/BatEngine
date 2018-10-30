db5dd82205df52a0a26aa71b8b338263ce808de6
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2018-10-30 12:51:05 +1100

    Fix world matrix multiplication

--- Engine/Model.h
+++ Engine/Model.h
@@ -17,10 +17,9 @@ public:
 		:
 		m_pDevice(pDevice),
 		m_pDeviceContext(pDeviceContext),
-		m_pShader(pShader)
-	{
-		UpdateWorldMatrix();
-	}
+		m_pShader(pShader),
+		m_matWorldMatrix(DirectX::XMMatrixIdentity())
+	{}
 	virtual ~Model() = default;
 
 	virtual void Draw( const DirectX::XMMATRIX& vp ) const = 0;
@@ -99,55 +98,3 @@ private:
 	IndexBuffer m_IndexBuffer;
 	Texture* m_pTexture;
 };
-
-/*template <typename V>
-class Model
-{
-public:
-	Model( ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext, const std::vector<V>& verts, const std::vector<int>& indices, D3D_PRIMITIVE_TOPOLOGY topology = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST )
-		:
-		m_Vertices( verts ),
-		m_Indices( indices ),
-		m_PrimitiveTopology( topology ),
-		m_IndexBuffer( pDevice, indices ),
-		m_VertexBuffer( pDevice, verts )
-	{}
-
-	Model( const Model& src ) = delete;
-	Model& operator=( const Model& src ) = delete;
-	Model( Model&& donor ) = delete;
-	Model& operator=( Model&& donor ) = delete;
-
-	void Draw()
-	{
-		m_VertexBuffer.Bind( m_pDeviceContext );
-		m_IndexBuffer.Bind( m_pDeviceContext );
-		m_pDeviceContext->IASetPrimitiveTopology( m_PrimitiveTopology );
-
-		m_pDeviceContext->DrawIndexed( GetIndexCount(), GetVertexCount(), 0 );
-	}
-
-	size_t GetIndexCount() const
-	{
-		return m_Indices.size();
-	}
-	size_t GetVertexCount() const
-	{
-		return m_Vertices.size();
-	}
-	D3D_PRIMITIVE_TOPOLOGY GetTopologyType() const
-	{
-		return m_PrimitiveTopology;
-	}
-private:
-private:
-	VertexBuffer<V> m_VertexBuffer;
-	IndexBuffer m_IndexBuffer;
-
-	D3D_PRIMITIVE_TOPOLOGY					m_PrimitiveTopology;
-	std::vector<V>							m_Vertices;
-	std::vector<int>						m_Indices;
-
-	ID3D11Device*							m_pDevice;
-	ID3D11DeviceContext*					m_pDeviceContext;
-};*/
