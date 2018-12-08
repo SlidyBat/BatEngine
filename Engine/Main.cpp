4d1556b4da493e62f08c665465b32216a51c6544
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2018-12-09 05:14:28 +1100

    Use a darker skybox
    Should make the effects to come stand out more

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -51,7 +51,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine,
 
 		FrameTimer ft;
 
-		auto pScene = std::make_unique<MarioTestScene>( wnd );
+		auto pScene = std::make_unique<CarTestScene>( wnd );
 		while( wnd.ProcessMessage() )
 		{
 			static int fpsCounter = 0;
