2339aaf104115d3726374c0231ebe80f4f98f62d
SlidyBat <rami.nouri@yahoo.com>
2018-12-27 05:39:59 +1100

    Improve PoolAllocator implementation
    Now allows for resizing/growing by using a chunk allocation system.

--- Engine/Util/PoolAllocator.h
+++ Engine/Util/PoolAllocator.h
@@ -4,35 +4,41 @@
 
 namespace Bat
 {
-	template <typename T>
+	template <typename T, size_t ChunkSize = 8192, bool GrowOnOverflow = true>
 	class PoolAllocator
 	{
 	public:
-		PoolAllocator( const size_t capacity )
+		// The allocator will ensure at least this capacity, but may
+		// also allocate more. Use Capacity() to check the real capacity.
+		PoolAllocator( const size_t capacity = 1 )
 		{
 			// we double up the memory as a linked list that stores 1 pointer per node
 			// so we need at least sizeof a pointer for this to work
 			static_assert(sizeof( T ) >= sizeof( char* ));
+			// not a good idea to have a size bigger than chunk size
+			static_assert( sizeof( T ) < ChunkSize );
 			assert( capacity > 0 );
 
 			if( capacity )
 			{
-				m_iCapacity = capacity;
-				m_pMemPool = new char[sizeof( T ) * capacity];
-				char* pCurrent = m_pMemPool;
-				m_pFreeList = pCurrent;
-				for( size_t i = 1; i < capacity; i++ )
+				const size_t nBytes = sizeof( T ) * capacity;
+				m_iCapacity = ChunkSize / nBytes;
+				const size_t nChunksNeeded = (nBytes / ChunkSize) + 1;
+				m_pChunks.resize( nChunksNeeded );
+				for( size_t i = 0; i < nChunksNeeded; i++ )
 				{
-					char* pNew = pCurrent + sizeof( T );
-					*(char**)pCurrent = pNew;
-					pCurrent = pNew;
+					m_pChunks[i] = new char[ChunkSize];
 				}
-				*(char**)pCurrent = nullptr;
+
+				m_pAllocHead = m_pChunks[0];
 			}
 		}
 		~PoolAllocator()
 		{
-			delete[] m_pMemPool;
+			for( char* pChunk : m_pChunks )
+			{
+				delete[] pChunk;
+			}
 
 #ifdef _DEBUG
 			if( m_iAllocs )
@@ -65,18 +71,30 @@ namespace Bat
 		template <typename... Args>
 		T* Alloc( Args&&... args )
 		{
-			if( !m_pFreeList )
+			char* pAlloc = AllocInternal();
+
+			if( !pAlloc )
 			{
-				assert( false && "Pool allocator ran out of memory" );
-				return nullptr;
+				if( GrowOnOverflow )
+				{
+					EnsureCapacity( m_iCapacity * GROW_MULTIPLIER ); // grow to accomodate
+					pAlloc = AllocInternal(); // try again
+					if( !pAlloc )
+					{
+						assert( false && "Failed to grow" );
+						return nullptr;
+					}
+				}
+				else
+				{
+					assert( false && "Ran out of memory" );
+					return nullptr;
+				}
 			}
 
 #ifdef _DEBUG
 			m_iAllocs++;
 #endif
-
-			char* pAlloc = m_pFreeList;
-			m_pFreeList = *(char**)m_pFreeList;
 			return new(pAlloc) T( std::forward<Args>( args )... );
 		}
 		void Free( T* pObject )
@@ -89,8 +107,67 @@ namespace Bat
 			m_iAllocs--;
 #endif
 		}
+
+		void EnsureCapacity( const size_t capacity )
+		{
+			if( m_iCapacity >= capacity )
+			{
+				return;
+			}
+
+			const size_t nBytes = capacity * sizeof( T );
+			const size_t nChunksNeeded = (nBytes / ChunkSize) + 1;
+			const size_t nOldBytes = m_iCapacity * sizeof( T );
+			const size_t nOldChunksNeeded = (nOldBytes / ChunkSize) + 1;
+
+			m_pChunks.resize( nChunksNeeded );
+			for( size_t i = nOldChunksNeeded; i < nChunksNeeded; i++ )
+			{
+				m_pChunks[i] = new char[ChunkSize];
+			}
+
+			m_iCapacity = capacity;
+		}
+
+		size_t Capacity() const { return m_iCapacity; }
 	private:
-		char* m_pMemPool = nullptr;
+		char* AllocInternal()
+		{
+			char* pAlloc = nullptr;
+
+			if( m_pFreeList )
+			{
+				pAlloc = m_pFreeList;
+				m_pFreeList = *(char**)m_pFreeList;
+			}
+			else
+			{
+				pAlloc = m_pAllocHead;
+
+				m_pAllocHead += sizeof( T );
+				const size_t nBytesUsedInChunk = m_pAllocHead - m_pChunks[m_iCurrentChunk];
+				if( nBytesUsedInChunk >= ChunkSize )
+				{
+					m_pAllocHead = m_pChunks[++m_iCurrentChunk]; // move on to next chunk
+
+					if( nBytesUsedInChunk > ChunkSize )
+					{
+						// there wasn't enough memory in that last chunk to do the allocation
+						pAlloc = m_pAllocHead;
+						// TODO: unlikely to happen, but should check for chunk change again
+						m_pAllocHead += sizeof( T );
+					}
+				}
+			}
+
+			return pAlloc;
+		}
+	private:
+		static constexpr size_t GROW_MULTIPLIER = 2;
+
+		std::vector<char*> m_pChunks;
+		size_t m_iCurrentChunk;
+		char* m_pAllocHead = nullptr;
 		char* m_pFreeList = nullptr;
 		size_t m_iCapacity = 0;
 #ifdef _DEBUG
