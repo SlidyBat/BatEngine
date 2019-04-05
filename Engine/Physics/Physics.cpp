d981f3d8c766222322f840a8abaf0e566f30986f
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-04-05 11:42:46 +1100

    Add PhysX cooking library
    Also some tiny amount of work on Physics interface

--- Engine/Physics/Physics.cpp
+++ Engine/Physics/Physics.cpp
@@ -13,6 +13,10 @@ namespace Bat
 {
 	static PxFoundation* g_pPxFoundation;
 	static PxPhysics*    g_pPxPhysics;
+	static PxScene*      g_pPxScene = nullptr;
+
+	static bool g_bFixedTimestep = false;
+	static float g_flFixedTimestep = 0.0f;
 
 	class BatPxErrorCallback : public PxErrorCallback
 	{
@@ -49,10 +53,50 @@ namespace Bat
 		}
 	};
 
+	class PxPhysicsMaterial : public IPhysicsMaterial
+	{
+	public:
+		PxPhysicsMaterial( PxMaterial* pMaterial )
+			:
+			m_pMaterial( pMaterial )
+		{
+			pMaterial->userData = this;
+		}
+
+		virtual float GetDynamicFriction() const override
+		{
+			return m_pMaterial->getDynamicFriction();
+		}
+	private:
+		PxMaterial* m_pMaterial;
+	};
+
+	static int Bat2PxGeometryType( PhysicsGeometryType type )
+	{
+		return (int)type;
+	}
+
+	static PhysicsGeometryType Px2BatGeometryType( int type )
+	{
+		return (PhysicsGeometryType)type;
+	}
+
+	static PxVec3 Bat2PxVec( const Vec3& vec )
+	{
+		return { vec.x, vec.y, vec.x };
+	}
+
+	static Vec3 Px2BatVec( const Vec3& vec )
+	{
+		return { vec.x, vec.y, vec.x };
+	}
+
 	void Physics::Initialize()
 	{
 		static BatPxErrorCallback px_error_callback;
 		static PxDefaultAllocator px_allocator_callback;
+
+		const PxTolerancesScale tolerances_scale;
 
 		g_pPxFoundation = PxCreateFoundation( PX_PHYSICS_VERSION, px_allocator_callback, px_error_callback );
 		if( !g_pPxFoundation )
@@ -61,16 +105,62 @@ namespace Bat
 		}
 
 		const bool record_mem_allocs = false;
-		g_pPxPhysics = PxCreatePhysics( PX_PHYSICS_VERSION, *g_pPxFoundation, PxTolerancesScale(), record_mem_allocs );
+		g_pPxPhysics = PxCreatePhysics( PX_PHYSICS_VERSION, *g_pPxFoundation, tolerances_scale, record_mem_allocs );
 		if( !g_pPxPhysics )
 		{
 			BAT_ABORT( "Failed to initialize PhysX physics object!" );
 		}
+
+		PxSceneDesc scene_desc( tolerances_scale );
+		scene_desc.gravity = { 0.0f, -9.8f, 0.0f };
+
+		//g_pPxScene = g_pPxPhysics->createScene( scene_desc );
 	}
 
 	void Physics::Shutdown()
 	{
 		g_pPxPhysics->release();
 		g_pPxFoundation->release();
+	}
+
+	void Physics::SetGravity( const Vec3& gravity )
+	{
+		g_pPxScene->setGravity( Bat2PxVec( gravity ) );
+	}
+
+	void Physics::EnableFixedTimestep( float deltatime )
+	{
+		g_flFixedTimestep = deltatime;
+		g_bFixedTimestep = true;
+	}
+
+	void Physics::DisableFixedTimestep()
+	{
+		g_bFixedTimestep = false;
+	}
+
+	void Physics::Simulate( float deltatime )
+	{
+		static float accumulator = 0.0f;
+
+		if( g_bFixedTimestep )
+		{
+			accumulator += deltatime;
+			while( accumulator >= g_flFixedTimestep )
+			{
+				g_pPxScene->simulate( g_flFixedTimestep );
+				accumulator -= g_flFixedTimestep;
+			}
+		}
+		else
+		{
+			g_pPxScene->simulate( deltatime );
+		}
+	}
+
+	IPhysicsMaterial* Physics::CreateMaterial(float static_friction, float dynamic_friction, float restitution)
+	{
+		return new PxPhysicsMaterial( g_pPxPhysics->createMaterial(static_friction, dynamic_friction, restitution) );
+		return nullptr;
 	}
 }
--- Engine/Physics/Physics.h
+++ Engine/Physics/Physics.h
@@ -2,10 +2,49 @@
 
 namespace Bat
 {
+	class IPhysicsGeometry;
+	class IPhysicsMaterial;
+
+	enum class PhysicsGeometryType
+	{
+		SPHERE,
+		PLANE,
+		CAPSULE,
+		BOX,
+		CONVEXMESH,
+		TRIANGLEMESH,
+		HEIGHTFIELD
+	};
+
 	class Physics
 	{
 	public:
 		static void Initialize();
 		static void Shutdown();
+
+		// Sets the gravity of the world. Default is { 0.0, -9.8, 0.0 }.
+		static void SetGravity( const Vec3& gravity );
+		// Enables fixed timestep with the value provided.
+		// Whenever `Simulate` is called it will accumulate the values passed in until
+		// it reaches the set fixed timestep value.
+		static void EnableFixedTimestep( float deltatime );
+		static void DisableFixedTimestep();
+
+		static void Simulate( float deltatime );
+
+		static IPhysicsMaterial* CreateMaterial(float static_friction, float dynamic_friction, float restitution);
+		static IPhysicsGeometry* CreateBoxShape();
+	};
+
+	class IPhysicsGeometry
+	{
+	public:
+		virtual PhysicsGeometryType GetType() const = 0;
+	};
+
+	class IPhysicsMaterial
+	{
+	public:
+		virtual float GetDynamicFriction() const = 0;
 	};
 }
