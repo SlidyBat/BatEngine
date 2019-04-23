d9eef8da0507471de0586e25cd920c46e3cd46d8
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-04-23 16:14:11 +1000

    Multiply ambient term by diffuse colour of material

--- Engine/Graphics/Shaders/LitGenericPS.hlsl
+++ Engine/Graphics/Shaders/LitGenericPS.hlsl
@@ -26,49 +26,23 @@ struct PixelInput
     float2 tex : TEXCOORD;
 };
 
-float3 DoLight( PixelInput input, float3 normal, Light light )
+float3 DoLight( Material material, float3 normal, float3 world_pos, Light light )
 {
-	float3 light_dir = light.Position - (float3)input.world_pos;
+	float3 light_dir = light.Position - world_pos;
 	float light_distance = length( light_dir );
 	light_dir /= light_distance; // normalize
 
 	float attenuation = 1.0f - smoothstep( light.Range * 0.75f, light.Range, light_distance );
 
-	float3 obj_diffuse = Mat.DiffuseColor.rgb;
-	if( Mat.HasDiffuseTexture )
-	{
-		if( any( obj_diffuse ) )
-		{
-			obj_diffuse *= DiffuseTexture.Sample( WrapSampler, input.tex ).rgb;
-		}
-		else
-		{
-			obj_diffuse = DiffuseTexture.Sample( WrapSampler, input.tex ).rgb;
-		}
-	}
-
-	float3 obj_specular = Mat.SpecularColor.rgb;
-	if( Mat.HasSpecularTexture )
-	{
-		if( any( obj_specular ) )
-		{
-			obj_specular *= SpecularTexture.Sample( WrapSampler, input.tex ).rgb;
-		}
-		else
-		{
-			obj_specular = SpecularTexture.Sample( WrapSampler, input.tex ).rgb;
-		}
-	}
-
 	// diffuse
 	float diff = saturate( dot( normal, light_dir ) );
-	float3 diffuse = light.Diffuse * (diff * obj_diffuse) * attenuation;
+	float3 diffuse = light.Diffuse * (diff * material.DiffuseColor.rgb) * attenuation;
 
 	// specular
-	float3 viewDir = normalize( Globals.CameraPos - (float3)input.world_pos );
+	float3 viewDir = normalize( Globals.CameraPos - world_pos );
 	float3 reflectDir = reflect( -light_dir, normal );
-	float spec = pow( saturate( dot( viewDir, reflectDir ) ), Mat.SpecularPower );
-	float3 specular = light.Specular * (spec * obj_specular) * attenuation;
+	float spec = pow( saturate( dot( viewDir, reflectDir ) ), material.SpecularPower );
+	float3 specular = light.Specular * (spec * material.SpecularColor.rgb) * attenuation;
 
 	float3 final = diffuse + specular;
 	
@@ -77,10 +51,10 @@ float3 DoLight( PixelInput input, float3 normal, Light light )
 
 float4 main( PixelInput input ) : SV_TARGET
 {
-	float3 world_pos = (float3)input.world_pos;
+	Material material = Mat;
 
 	float3 normal = input.normal;
-	if( Mat.HasNormalTexture )
+	if( material.HasNormalTexture )
 	{
 		normal = NormalTexture.Sample( WrapSampler, input.tex ).xyz;
 		normal = normalize( (normal * 2.0f) - 1.0f );
@@ -88,25 +62,37 @@ float4 main( PixelInput input ) : SV_TARGET
 		normal = normalize( mul( normal, tbn ) );
 	}
 
+	// get specular colour of material
+	if( material.HasSpecularTexture )
+	{
+		material.SpecularColor = SpecularTexture.Sample( WrapSampler, input.tex );
+	}
+
+	if( material.HasDiffuseTexture )
+	{
+		material.DiffuseColor = DiffuseTexture.Sample( WrapSampler, input.tex );
+	}
+
 	// ambient
-	float3 ambient = Mat.AmbientColor.rgb;
-	if( Mat.HasAmbientTexture )
+	float3 ambient = material.AmbientColor.rgb;
+	if( material.HasAmbientTexture )
 	{
 		if( any( ambient ) )
 		{
-			ambient *= Mat.AmbientColor.rgb;
+			ambient *= material.AmbientColor.rgb;
 		}
 		else
 		{
-			ambient = Mat.AmbientColor.rgb;
+			ambient = material.AmbientColor.rgb;
 		}
 	}
 
-	ambient *= Mat.GlobalAmbient;
+	ambient *= material.GlobalAmbient.rgb;
+	ambient *= material.DiffuseColor.rgb;
 
 	// emissive
-	float3 emissive = Mat.EmissiveColor.rgb;
-	if( Mat.HasEmissiveTexture )
+	float3 emissive = material.EmissiveColor.rgb;
+	if( material.HasEmissiveTexture )
 	{
 		if( any( emissive ) )
 		{
@@ -121,7 +107,7 @@ float4 main( PixelInput input ) : SV_TARGET
 	float3 colour = 0.0f;
 	for( uint i = 0; i < NumLights; i++ )
 	{
-		colour += DoLight( input, normal, Lights[i] );
+		colour += DoLight( material, normal, input.world_pos.xyz, Lights[i] );
 	}
 
 	colour += ambient + emissive;
