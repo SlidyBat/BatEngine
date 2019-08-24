212ee2feb0c18495bcc9538c48096ae001765796
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2019-08-24 11:52:04 +1000

    Fix FlushMessages crashes

--- Engine/Util/COMException.h
+++ Engine/Util/COMException.h
@@ -7,8 +7,8 @@
 #include <comdef.h>
 
 #ifdef _DEBUG
-#define THROW_COM_ERROR( hr, msg ) ASSERT( false, COMException( hr, msg, __FILE__, __FUNCTION__, __LINE__ ).what() )
-#define COM_THROW_IF_FAILED( hr ) ASSERT( SUCCEEDED( hr ), COMException( hr, "Error", __FILE__, __FUNCTION__, __LINE__ ).what() )
+#define THROW_COM_ERROR( hr, msg ) ASSERT( false, COMException( hr, msg, __FILE__, __FUNCTION__, __LINE__ ).WhatWithoutFile() )
+#define COM_THROW_IF_FAILED( hr ) ASSERT( SUCCEEDED( hr ), COMException( hr, "Error", __FILE__, __FUNCTION__, __LINE__ ).WhatWithoutFile() )
 #else
 #define THROW_COM_ERROR( hr, msg ) throw COMException( hr, msg, __FILE__, __FUNCTION__, __LINE__ )
 #define COM_THROW_IF_FAILED( hr ) do { if( FAILED( hr ) ) throw COMException( hr, __FILE__, __FUNCTION__, __LINE__ ); } while( false )
@@ -41,10 +41,12 @@ public:
 		_com_error error( hr );
 		whatmsg = error.ErrorMessage();
 		whatmsg += '\n';
-		whatmsg += '\n';
-		whatmsg += "File: " + file + '\n';
-		whatmsg += "Function: " + function + '\n';
-		whatmsg += "Line: " + std::to_string( line ) + '\n';
+
+		filemsg = "";
+		filemsg += '\n';
+		filemsg += "File: " + file + '\n';
+		filemsg += "Function: " + function + '\n';
+		filemsg += "Line: " + std::to_string( line ) + '\n';
 	}
 
 	COMException( HRESULT hr, const std::string& message, const std::string& file, const std::string& function, int line )
@@ -55,19 +57,27 @@ public:
 		whatmsg = "Error: " + message + '\n';
 		whatmsg += error.ErrorMessage();
 		whatmsg += '\n';
-		whatmsg += '\n';
-		whatmsg += "File: " + file + '\n';
-		whatmsg += "Function: " + function + '\n';
-		whatmsg += "Line: " + std::to_string( line ) + '\n';
+
+		filemsg = "";
+		filemsg += '\n';
+		filemsg += "File: " + file + '\n';
+		filemsg += "Function: " + function + '\n';
+		filemsg += "Line: " + std::to_string( line ) + '\n';
 	}
 
 	HRESULT GetHResult() const { return hr; }
 
-	virtual const char* what() const override
+	const char* WhatWithoutFile() const
 	{
 		return whatmsg.c_str();
+	}
+
+	virtual const char* what() const override
+	{
+		return (whatmsg + filemsg).c_str();
 	}
 private:
 	HRESULT hr;
 	std::string whatmsg;
+	std::string filemsg;
 };
