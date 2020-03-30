fca907b185e0a0e3f08372feaec2810ab34f9e7f
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2020-03-30 17:59:13 +1100

    Add convenience File class

--- Engine/Util/FileSystem.cpp
+++ Engine/Util/FileSystem.cpp
@@ -5,62 +5,50 @@ namespace Bat
 {
 	FileHandle_t FileSystem::Open( const char* filename, const char* mode )
 	{
-		BAT_LOG( "Opening %s (%s)", filename, mode );
-
-		FILE* file;
+		FILE* file = nullptr;
 		fopen_s( &file, filename, mode );
 		return (FileHandle_t)file;
 	}
-
 	void FileSystem::Close( FileHandle_t handle )
 	{
 		fclose( (FILE*)handle );
 	}
-
 	size_t FileSystem::Read( FileHandle_t handle, char* out, size_t size )
 	{
 		return fread( out, 1, size, (FILE*)handle );
 	}
-
 	Future<size_t> FileSystem::ReadAsync( FileHandle_t handle, char* out, size_t size )
 	{
 		return std::async( &FileSystem::Read, this, handle, out, size );
 	}
-
 	size_t FileSystem::Write( FileHandle_t handle, const char* in, size_t size )
 	{
 		return fwrite( in, 1, size, (FILE*)handle );
 	}
-
 	Future<size_t> FileSystem::WriteAsync( FileHandle_t handle, const char* in, size_t size )
 	{
 		return std::async( &FileSystem::Write, this, handle, in, size );
 	}
-
 	void FileSystem::Flush( FileHandle_t handle )
 	{
 		fflush( (FILE*)handle );
 	}
-
 	void FileSystem::Seek( FileHandle_t handle, int off, SeekPos pos )
 	{
 		fseek( (FILE*)handle, off, (int)pos );
 	}
-
-	int FileSystem::Tell( FileHandle_t handle )
+	int FileSystem::Tell( FileHandle_t handle ) const
 	{
 		return ftell( (FILE*)handle );
 	}
-
-	bool FileSystem::Exists( const char* filename )
+	bool FileSystem::Exists( const char* filename ) const
 	{
 		struct stat s;
 		int rc = stat( filename, &s );
 
 		return rc == 0;
 	}
-
-	size_t FileSystem::Size( FileHandle_t handle )
+	size_t FileSystem::Size( FileHandle_t handle ) const
 	{
 		auto file = (FILE*)handle;
 
@@ -71,35 +59,29 @@ namespace Bat
 
 		return size;
 	}
-
-	size_t FileSystem::Size( const char* filename )
+	size_t FileSystem::Size( const char* filename ) const
 	{
 		struct stat s;
 		int rc = stat( filename, &s );
 
 		return rc == 0 ? (size_t)s.st_size : (size_t)-1;
 	}
-
 	std::string FileSystem::ReadAllText( const char* filename )
 	{
 		return ReadAll( filename, "r" );
 	}
-
 	Future<std::string> FileSystem::ReadAllTextAsync( const char* filename )
 	{
 		return std::async( &FileSystem::ReadAllText, this, filename );
 	}
-
 	std::string FileSystem::ReadAllBinary( const char* filename )
 	{
 		return ReadAll( filename, "rb" );
 	}
-
 	Future<std::string> FileSystem::ReadAllBinaryAsync( const char* filename )
 	{
 		return std::async( &FileSystem::ReadAllBinary, this, filename );
 	}
-
 	std::string FileSystem::ReadAll( const char* filename, const char* mode )
 	{
 		size_t filesize = Size( filename );
@@ -112,5 +94,67 @@ namespace Bat
 		Close( handle );
 
 		return buf;
+	}
+
+	File::File( FileSystem& filesystem, const char* filename, const char* mode )
+		:
+		m_pFileSystem( &filesystem )
+	{
+		m_hFile = m_pFileSystem->Open( filename, mode );
+	}
+	File::~File()
+	{
+		if( m_hFile != INVALID_FILE_HANDLE )
+		{
+			m_pFileSystem->Close( m_hFile );
+		}
+	}
+	File::File( File&& donor )
+	{
+		*this = std::move( donor );
+	}
+	File& File::operator=( File&& donor )
+	{
+		std::swap( m_hFile, donor.m_hFile );
+		std::swap( m_pFileSystem, donor.m_pFileSystem );
+
+		return *this;
+	}
+	void File::Close()
+	{
+		m_pFileSystem->Close( m_hFile );
+		m_hFile = INVALID_FILE_HANDLE;
+	}
+	size_t File::Read( char* out, size_t size )
+	{
+		return m_pFileSystem->Read( m_hFile, out, size );
+	}
+	Future<size_t> File::ReadAsync( char* out, size_t size )
+	{
+		return m_pFileSystem->ReadAsync( m_hFile, out, size );
+	}
+	size_t File::Write( const char* in, size_t size )
+	{
+		return m_pFileSystem->Write( m_hFile, in, size );
+	}
+	Future<size_t> File::WriteAsync( const char* in, size_t size )
+	{
+		return m_pFileSystem->WriteAsync( m_hFile, in, size );
+	}
+	void File::Flush()
+	{
+		m_pFileSystem->Flush( m_hFile );
+	}
+	size_t File::Size() const
+	{
+		return m_pFileSystem->Size( m_hFile );
+	}
+	void File::Seek( int off, SeekPos pos )
+	{
+		m_pFileSystem->Seek( m_hFile, off, pos );
+	}
+	int File::Tell() const
+	{
+		return m_pFileSystem->Tell( m_hFile );
 	}
 }
--- Engine/Util/FileSystem.h
+++ Engine/Util/FileSystem.h
@@ -8,6 +8,7 @@
 namespace Bat
 {
 	using FileHandle_t = intptr_t;
+	static constexpr FileHandle_t INVALID_FILE_HANDLE = 0;
 
 	enum class SeekPos
 	{
@@ -29,12 +30,12 @@ namespace Bat
 		void Flush( FileHandle_t handle );
 
 		void Seek( FileHandle_t handle, int off, SeekPos pos );
-		int Tell( FileHandle_t handle );
+		int Tell( FileHandle_t handle ) const;
 
-		bool Exists( const char* filename );
+		bool Exists( const char* filename ) const;
 
-		size_t Size( FileHandle_t handle );
-		size_t Size( const char* filename );
+		size_t Size( FileHandle_t handle ) const;
+		size_t Size( const char* filename ) const;
 
 		std::string ReadAllText( const char* filename );
 		Future<std::string> ReadAllTextAsync( const char* filename );
@@ -42,5 +43,32 @@ namespace Bat
 		Future<std::string> ReadAllBinaryAsync( const char* filename );
 	private:
 		std::string ReadAll( const char* filename, const char* mode );
+	};
+
+	class File
+	{
+	public:
+		File( FileSystem& filesystem, const char* filename, const char* mode );
+		~File();
+		File( const File& ) = delete;
+		File& operator=( const File& ) = delete;
+		File( File&& donor );
+		File& operator=( File&& donor );
+		
+		void Close();
+
+		size_t Read( char* out, size_t size );
+		Future<size_t> ReadAsync( char* out, size_t size );
+		size_t Write( const char* in, size_t size );
+		Future<size_t> WriteAsync( const char* in, size_t size );
+		void Flush();
+
+		size_t Size() const;
+
+		void Seek( int off, SeekPos pos );
+		int Tell() const;
+	private:
+		FileHandle_t m_hFile = INVALID_FILE_HANDLE;
+		FileSystem* m_pFileSystem = nullptr;
 	};
 }
